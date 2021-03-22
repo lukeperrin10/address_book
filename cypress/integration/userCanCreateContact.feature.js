da7ac380a5904e152f9c6db3ba562aac442a3aca
lukeperrin10 <lukeperrin27@gmail.com>
2021-03-22 21:32:09 +0100

    added tests for contact input, aswell as added for to html file. all pass correctly.

--- cypress/integration/userCanCreateContact.feature.js
+++ cypress/integration/userCanCreateContact.feature.js
@@ -4,5 +4,13 @@ describe('user can create a contact', () => {
     it('test', () => {
         cy.visit('http://localhost:3002')
         cy.get('#add-contact').click()
+        cy.get('#first_name').type('Luke')
+        cy.get('#last_name').type('Perrin')
+        cy.get('#telephone').type('0731234567')
+        cy.get('#company').type('Student')
+        cy.get('#twitter').type('welsh_thor')
+        
     })
+
+
 })
