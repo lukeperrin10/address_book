18ceed11cefcb41e798b239c709a20c8eec7569b
lukeperrin10 <lukeperrin27@gmail.com>
2021-03-22 21:42:51 +0100

    added test for contact information

--- cypress/integration/userCanCreateContact.feature.js
+++ cypress/integration/userCanCreateContact.feature.js
@@ -9,8 +9,19 @@ describe('user can create a contact', () => {
         cy.get('#telephone').type('0731234567')
         cy.get('#company').type('Student')
         cy.get('#twitter').type('welsh_thor')
-        
+        cy.get('#submit').click()
     })
+
+    it('displays a name of the new contact', () => {
+        cy.get('#contact-list').should('contain','Luke')
+    })
+
+    it('displays the telephone number for the new contact', () => {
+        cy.get('#contact-list').should('contain','0731234567')
+    })
+
+        
+    
 
 
 })
